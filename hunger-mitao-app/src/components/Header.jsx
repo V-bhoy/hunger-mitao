fb5dcc2e802c649a281eaa8bb2bab379896a2663
VBhoy <83490034+V-bhoy@users.noreply.github.com>
2025-04-26 19:00:46 +0530

    initiate app

---
+++ hunger-mitao-app/src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header(){
+    return <div className={"header"}>
+        <div className={"logo"}>
+            <img/>
+        </div>
+        <div className={"nav-list"}>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>
+}
